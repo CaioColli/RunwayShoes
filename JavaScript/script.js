4f85515ae7ac2bc71e995ae9f346bc992bfc0bde
Caio Colli <110783734+CaioColli@users.noreply.github.com>
2023-05-04 14:57:10 -0300

    Add files via upload

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -224,23 +224,27 @@ btnBuyModal_4_flash.addEventListener('click', function () {
 
 // Show More New Products
 
-const btnShowMore = document.querySelector('.new_products_btn_showMore')
-const moreNewProducts = document.querySelector('.new_products_showMore')
-const btnSeeLess = document.querySelector('.new_products_btn_seeLess')
+const btnShowMoreNewProducts = document.querySelector('#new_products_btn_showMore_newProducts')
+const moreNewProducts = document.querySelector('#new_products_showMore_newProducts')
+const btnSeeLessNewProducts = document.querySelector('#new_products_btn_seeLess_newProducts')
 
-btnShowMore.addEventListener('click', function () {
-    btnShowMore.style.display = 'none'
+btnShowMoreNewProducts.addEventListener('click', function () {
+    btnShowMoreNewProducts.style.display = 'none'
     moreNewProducts.style.left = '0'
     moreNewProducts.style.position = "relative"
 })
 
-btnSeeLess.addEventListener('click', function () {
+btnSeeLessNewProducts.addEventListener('click', function () {
     moreNewProducts.style.left = '-21.875rem'
     setTimeout(function () {
         moreNewProducts.style.position = 'absolute'
     }, 950)
-    btnShowMore.style.display = 'inline-block'
+    setTimeout(function(){
+        btnShowMoreNewProducts.style.display = 'inline-block'
+    }, 980)
 })
+
+// Show More New Products End
 
 // Modal Of New Products
 
@@ -341,15 +345,16 @@ btnClose_3_new.addEventListener('click', function () {
     }, 950)
 })
 
-btnBuyModal_3_new.addEventListener('click', function(){
+btnBuyModal_3_new.addEventListener('click', function () {
     modal_3_new.style.left = '-21.875rem'
-    setTimeout(function(){
+    setTimeout(function () {
         modal_3_new.style.position = 'absolute'
     }, 950)
 })
 // Modal 3 End
+// Modal Of New Products End
 
-// Modal Of New Products
+// Modal Of More New Products
 // Modal 1
 const btnBuy_1_new_more = document.querySelector('#more_new_product_content_btn_buyProduct_1')
 const btnClose_1_new_more = document.querySelector('#more_new_product_modal_iconClose_1')
@@ -457,4 +462,245 @@ btnBuyModal_3_new_more.addEventListener('click', function () {
 // Modal 3 End
 
 // Modal End
-// Modal Of New Products
+// Modal Of New Products End
+
+// Modal Of Sports Products
+
+// Modal 1
+const btnBuy_1_sports = document.querySelector('#sports_product_content_btn_buyProduct_1')
+const btnClose_1_sports = document.querySelector('#sports_product_modal_iconClose_1')
+const modal_1_sports = document.querySelector('#sports_product_modal_1')
+const btnBuyModal_1_sports = document.querySelector('#sports_card__btnBuy_1')
+
+btnBuy_1_sports.addEventListener('click', function () {
+    modal_1_sports.style.position = 'relative'
+    modal_1_sports.style.left = '0'
+    modal_1_sports.style.marginBottom = '1rem'
+    document.addEventListener('click', function (event) {
+        if (!modal_1_sports.contains(event.target) && !btnBuy_1_sports.contains(event.target)) {
+            modal_1_sports.style.left = '-21.875rem'
+            setTimeout(function () {
+                modal_1_sports.style.position = 'absolute'
+            }, 950)
+        }
+    })
+})
+
+btnClose_1_sports.addEventListener('click', function () {
+    modal_1_sports.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_1_sports.style.position = 'absolute'
+    }, 950)
+})
+
+btnBuyModal_1_sports.addEventListener('click', function () {
+    modal_1_sports.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_1_sports.style.position = 'absolute'
+    }, 950)
+})
+// Modal 1 End
+
+// Modal 2
+const btnBuy_2_sports = document.querySelector('#sports_product_content_btn_buyProduct_2')
+const btnClose_2_sports = document.querySelector('#sports_product_modal_iconClose_2')
+const modal_2_sports = document.querySelector('#sports_product_modal_2')
+const btnBuyModal_2_sports = document.querySelector('#sports_card__btnBuy_2')
+
+btnBuy_2_sports.addEventListener('click', function () {
+    modal_2_sports.style.position = 'relative'
+    modal_2_sports.style.left = '0'
+    modal_2_sports.style.marginBottom = '1rem'
+    document.addEventListener('click', function (event) {
+        if (!modal_2_sports.contains(event.target) && !btnBuy_2_sports.contains(event.target)) {
+            modal_2_sports.style.left = '-21.875rem'
+            setTimeout(function () {
+                modal_2_sports.style.position = 'absolute'
+            }, 950)
+        }
+    })
+})
+
+btnClose_2_sports.addEventListener('click', function () {
+    modal_2_sports.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_2_sports.style.position = 'absolute'
+    }, 950)
+})
+
+btnBuyModal_2_sports.addEventListener('click', function () {
+    modal_2_sports.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_2_sports.style.position = 'absolute'
+    }, 950)
+})
+// Modal 2 End
+
+
+// Modal 3
+const btnBuy_3_sports = document.querySelector('#sports_product_content_btn_buyProduct_3')
+const btnClose_3_sports = document.querySelector('#sports_product_modal_iconClose_3')
+const modal_3_sports = document.querySelector('#sports_product_modal_3')
+const btnBuyModal_3_sports = document.querySelector('#sports_card__btnBuy_3')
+
+btnBuy_3_sports.addEventListener('click', function () {
+    modal_3_sports.style.position = 'relative'
+    modal_3_sports.style.left = '0'
+    modal_3_sports.style.marginBottom = '1rem'
+    document.addEventListener('click', function (event) {
+        if (!modal_3_sports.contains(event.target) && !btnBuy_3_sports.contains(event.target)) {
+            modal_3_sports.style.left = '-21.875rem'
+            setTimeout(function () {
+                modal_3_sports.style.position = 'absolute'
+            }, 950)
+        }
+    })
+})
+
+btnClose_3_sports.addEventListener('click', function () {
+    modal_3_sports.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_3_sports.style.position = 'absolute'
+    }, 950)
+})
+
+btnBuyModal_3_sports.addEventListener('click', function () {
+    modal_3_sports.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_3_sports.style.position = 'absolute'
+    }, 950)
+})
+// Modal 3 End
+
+// Show More Sports Products
+
+const btnShowMoreSports = document.querySelector('#sports_products_btn_showMore')
+const moreSportsProducts = document.querySelector('#sports_products_showMore')
+const btnSeeLessSportsProducts = document.querySelector('#sports_products_btn_seeLess_newProducts')
+
+btnShowMoreSports.addEventListener('click', function () {
+    btnShowMoreSports.style.display = 'none'
+    moreSportsProducts.style.left = '0'
+    moreSportsProducts.style.position = "relative"
+})
+
+btnSeeLessSportsProducts.addEventListener('click', function () {
+    moreSportsProducts.style.left = '-21.875rem'
+    setTimeout(function () {
+        moreSportsProducts.style.position = 'absolute'
+    }, 950)
+    setTimeout(function(){
+        btnShowMoreSports.style.display = 'inline-block'
+    }, 980)
+})
+
+// Show More New Products End
+
+// Modal Of More Sports Products
+// Modal 1
+const btnBuy_1_sports_more = document.querySelector('#more_sports_product_content_btn_buyProduct_1')
+const btnClose_1_sports_more = document.querySelector('#more_sports_product_modal_iconClose_1')
+const modal_1_sports_more = document.querySelector('#more_sports_product_modal_1')
+const btnBuyModal_1_sports_more = document.querySelector('#more_sports_card__btnBuy_1')
+
+btnBuy_1_sports_more.addEventListener('click', function () {
+    modal_1_sports_more.style.position = "relative"
+    modal_1_sports_more.style.left = '0'
+    modal_1_sports_more.style.marginBottom = '1rem'
+
+    document.addEventListener("click", function (event) {
+        if (!modal_1_sports_more.contains(event.target) && !btnBuy_1_sports_more.contains(event.target)) {
+            modal_1_sports_more.style.left = '-21.875rem'
+            setTimeout(function () {
+                modal_1_sports_more.style.position = 'absolute'
+            }, 950)
+        }
+    })
+})
+
+btnClose_1_sports_more.addEventListener('click', function () {
+    modal_1_sports_more.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_1_sports_more.style.position = "absolute"
+    }, 950)
+})
+
+btnBuyModal_1_sports_more.addEventListener('click', function () {
+    modal_1_sports_more.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_1_sports_more.style.position = 'absolute'
+    }, 950)
+})
+// Modal 1 End
+
+// Modal 2
+const btnBuy_2_sports_more = document.querySelector('#more_sports_product_content_btn_buyProduct_2')
+const btnClose_2_sports_more = document.querySelector('#more_sports_product_modal_iconClose_2')
+const modal_2_sports_more = document.querySelector('#more_sports_product_modal_2')
+const btnBuyModal_2_sports_more = document.querySelector('#more_sports_card__btnBuy_2')
+
+btnBuy_2_sports_more.addEventListener('click', function () {
+    modal_2_sports_more.style.position = "relative"
+    modal_2_sports_more.style.left = '0'
+    modal_2_sports_more.style.marginBottom = '1rem'
+
+    document.addEventListener("click", function (event) {
+        if (!modal_2_sports_more.contains(event.target) && !btnBuy_2_sports_more.contains(event.target)) {
+            modal_2_sports_more.style.left = '-21.875rem'
+            setTimeout(function () {
+                modal_2_sports_more.style.position = 'absolute'
+            }, 950)
+        }
+    })
+})
+
+btnClose_2_sports_more.addEventListener('click', function () {
+    modal_2_sports_more.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_2_sports_more.style.position = "absolute"
+    }, 950)
+})
+
+btnBuyModal_2_sports_more.addEventListener('click', function () {
+    modal_2_sports_more.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_2_sports_more.style.position = 'absolute'
+    }, 950)
+})
+// Modal 2 End
+
+// Modal 2
+const btnBuy_3_sports_more = document.querySelector('#more_sports_product_content_btn_buyProduct_3')
+const btnClose_3_sports_more = document.querySelector('#more_sports_product_modal_iconClose_3')
+const modal_3_sports_more = document.querySelector('#more_sports_product_modal_3')
+const btnBuyModal_3_sports_more = document.querySelector('#more_sports_card__btnBuy_3')
+
+btnBuy_3_sports_more.addEventListener('click', function () {
+    modal_3_sports_more.style.position = "relative"
+    modal_3_sports_more.style.left = '0'
+    modal_3_sports_more.style.marginBottom = '1rem'
+
+    document.addEventListener("click", function (event) {
+        if (!modal_3_sports_more.contains(event.target) && !btnBuy_3_sports_more.contains(event.target)) {
+            modal_3_sports_more.style.left = '-21.875rem'
+            setTimeout(function () {
+                modal_3_sports_more.style.position = 'absolute'
+            }, 950)
+        }
+    })
+})
+
+btnClose_3_sports_more.addEventListener('click', function () {
+    modal_3_sports_more.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_3_sports_more.style.position = "absolute"
+    }, 950)
+})
+
+btnBuyModal_3_sports_more.addEventListener('click', function () {
+    modal_3_sports_more.style.left = '-21.875rem'
+    setTimeout(function () {
+        modal_3_sports_more.style.position = 'absolute'
+    }, 950)
+})
+// Modal 2 End
